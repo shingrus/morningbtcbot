1b49638e7e653164c797b9f29564c59b8c071ec2
shingrus <shingrus@gmail.com>
2018-02-02 19:36:01 +0300

    remove ring make own circle array

--- stat.go
+++ stat.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-	"container/ring"
 	"github.com/montanaflynn/stats"
+	"sync"
 )
 
+const storegeSize = 5
+
 type Stat struct {
-	liteStorage *ring.Ring
+	mut         sync.Mutex
+	liteStorage []float64
+	pointer     int
+	length      int
 }
 
 func InitStat() (stat *Stat) {
-	stat = &Stat{ring.New(1000)}
-	return
+	return &Stat{liteStorage:make([]float64,storegeSize)}
+
 }
 
 func (s *Stat) AddStat(f float32) {
-	s.liteStorage.Value = float64(f)
-	s.liteStorage = s.liteStorage.Next()
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	s.liteStorage[s.pointer] = float64(f)
+	if(s.length <storegeSize) {
+		s.length++
+	}
+	s.pointer++
+	s.pointer%=storegeSize
 }
 
 func (s *Stat) getMedian() (float64) {
-	var values []float64
-	s.liteStorage.Do(func(elem interface{}) {
-		if elem != nil {
-			values = append(values, elem.(float64))
-		}
-	})
-	if len(values) > 0 {
-		ret, err := stats.Median(values)
+
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	if s.length > 0 {
+		ret, err := stats.Median(s.liteStorage[:s.length])
 		if err != nil {
 			return 0
 		}
